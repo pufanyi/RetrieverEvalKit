ebc014150b0e9bb32961a8d207a7a401bbe4d4b0
pufanyi <FPU001@e.ntu.edu.sg>
2025-09-18 01:08:37 +0800

    lint

--- img_search/data/inquire.py
+++ img_search/data/inquire.py
@@ -1,13 +1,15 @@
-from typing import Iterator
+from collections.abc import Iterator
 
-from PIL import Image
 from datasets import load_dataset
+from PIL import Image
+
 from .dataset import ImageDataset
+
 
 class InquireDataset(ImageDataset):
     def __init__(self, path: str = "evendrow/INQUIRE-Rerank"):
         self.dataset = load_dataset(path)
-    
+
     def length(self) -> int:
         return len(self.dataset)
 
