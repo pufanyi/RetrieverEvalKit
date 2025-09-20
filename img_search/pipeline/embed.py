41fed47c58a9406f64d06130e7aaf1a99dc831f0
pufanyi <FPU001@e.ntu.edu.sg>
2025-09-20 22:17:19 +0800

    Add configuration files and embed pipeline setup for siglip2 model

--- img_search/pipeline/embed.py
+++ img_search/pipeline/embed.py
@@ -1,2 +1,11 @@
-def run():
-    pass
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+
+@hydra.main(config_path="../../config", version_base=None, config_name="config")
+def main(cfg: DictConfig):
+    print(OmegaConf.to_yaml(cfg))
+
+
+if __name__ == "__main__":
+    main()
