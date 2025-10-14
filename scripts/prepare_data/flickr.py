79fc7769f63cf9e33070e3a4696955320ae89bc3
pufanyi <pufanyi@sensetime.com>
2025-10-14 12:33:41 +0000

    Add data preparation script for Flickr30k dataset with image saving and captioning

--- scripts/prepare_data/flickr.py
+++ scripts/prepare_data/flickr.py
@@ -1,5 +1,17 @@
+import json
+from pathlib import Path
+
 from datasets import load_dataset
+from tqdm import tqdm
 
 if __name__ == "__main__":
+    output_path = Path("data/flickr30k")
     dataset = load_dataset("lmms-lab/flickr30k")
-    print(dataset)
+    image_path = output_path / "images"
+    image_path.mkdir(parents=True, exist_ok=True)
+    with open(output_path / "captions.jsonl", "w") as f:
+        for item in tqdm(dataset["test"], desc="Processing dataset"):
+            file_name = item["filename"]
+            item["image"].save(image_path / file_name)
+            caption = {"caption": item["caption"], "image": file_name}
+            f.write(json.dumps(caption) + "\n")
