3d263ec8a940a5e1a8a016b1d0c23ddc6e38a38f
pufanyi <pufanyi@sensetime.com>
2025-10-16 11:26:17 +0000

    Refactor import statements and format dataset loading in flickr30k_fix.py

--- scripts/prepare_data/flickr30k_fix.py
+++ scripts/prepare_data/flickr30k_fix.py
@@ -1,9 +1,10 @@
-from datasets import load_dataset
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 from tqdm.rich import tqdm
 
 if __name__ == "__main__":
-    dataset = load_dataset("pufanyi/flickr30k-jina-embeddings-v4", "images", split="train")
+    dataset = load_dataset(
+        "pufanyi/flickr30k-jina-embeddings-v4", "images", split="train"
+    )
     data = {}
     for item in tqdm(dataset):
         data[item["id"]] = item
